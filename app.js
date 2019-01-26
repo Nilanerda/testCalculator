8d055667397a57fee37b563a0ca421514ecf2eae
Nilanerda <zzizzu@ukr.net>
2019-01-26 12:09:58 +0200

    Refactor code, rewrite html onclick

--- app.js
+++ app.js
@@ -1,23 +1,51 @@
-function insert(num) {
-    document.form.textview.value = document.form.textview.value + num;
-}
+// Full clean
 
-function equal() {
-    var exp = document.form.textview.value;
-    if(exp) {
-        document.form.textview.value = eval(exp);
+var textClear = document.getElementById('textClear');
+textClear.addEventListener('click', clear);
+
+    function clear() {
+        document.form.textview.value = '';
     }
-}
 
-function clean() {
-    document.form.textview.value = '';
-}
+//  Add negative symbol
 
-function back() {
-    var exp = document.form.textview.value;
-    document.form.textview.value = exp.substring(0, exp.length-1);
-}
+var addNegativeSymbol = document.getElementById('negativeSymbol');
+addNegativeSymbol.addEventListener('click', negative);
 
-function negative() {
-    document.form.textview.value = - document.form.textview.value;
-}
+    function negative() {
+        document.form.textview.value = - document.form.textview.value;
+    }
+
+// Add equally
+
+var equal = document.getElementById('equal');
+equal.addEventListener('click', equally);
+
+    function equally() {
+        var eqv = document.form.textview.value;
+        if(eqv) {
+            document.form.textview.value = eval(eqv);
+        }
+    }
+
+// Backspace function
+
+var backClear = document.getElementById('back');
+backClear.addEventListener('click', backspace);
+
+    function backspace() {
+        var lng = document.form.textview.value;
+        document.form.textview.value = lng.substring(0, lng.length-1);
+    }
+
+// Make relations between buttons and text input
+
+var numbers = document.getElementsByClassName('numbers');
+
+    function insertNumber(event) {
+        document.form.textview.value += event.target.value;
+    }
+
+    for (i = 0; i < numbers.length; i++) {
+        numbers[i].addEventListener('click', insertNumber);
+    }
